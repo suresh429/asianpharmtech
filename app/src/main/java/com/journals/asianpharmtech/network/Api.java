a1a95686a6eb49cf261b340b2de88396c7c14bf0
suresh <ksuresh.unique@gmail.com>
2020-11-02 19:12:32 +0530

    intial

---
+++ app/src/main/java/com/journals/asianpharmtech/network/Api.java
@@ -0,0 +1,66 @@
+package com.journals.asianpharmtech.network;
+
+import com.google.gson.JsonObject;
+import com.journals.asianpharmtech.model.AbstractResponse;
+import com.journals.asianpharmtech.model.ArchiveResponse;
+import com.journals.asianpharmtech.model.CategoryResponse;
+import com.journals.asianpharmtech.model.ContactResponse;
+import com.journals.asianpharmtech.model.CurrentIssueResponse;
+import com.journals.asianpharmtech.model.EditorialBoardResponse;
+import com.journals.asianpharmtech.model.InPressResponse;
+import com.journals.asianpharmtech.model.JournalHomeResponse;
+import com.journals.asianpharmtech.model.JournalsListResponse;
+import com.journals.asianpharmtech.model.VolumeIssueResponse;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+public interface Api {
+
+
+
+    @Headers("Content-Type: application/json")
+    @POST("journalslistapi.php")
+    Call<CategoryResponse> getCategoryList(@Body JsonObject jsonObject);
+
+    @Headers("Content-Type: application/json")
+    @POST("aboutjournalapi.php")
+    Call<JournalHomeResponse> getJournalHomeDetails(@Body JsonObject jsonObject);
+
+    @Headers("Content-Type: application/json")
+    @POST("current-issueapi.php")
+    Call<CurrentIssueResponse> getCurrentIssueList(@Body JsonObject jsonObject);
+
+    @Headers("Content-Type: application/json")
+    @POST("inpressapi.php")
+    Call<InPressResponse> getInPressList(@Body JsonObject jsonObject);
+
+    @Headers("Content-Type: application/json")
+    @POST("archiveapi.php")
+    Call<ArchiveResponse> getArchiveList(@Body JsonObject jsonObject);
+
+    @Headers("Content-Type: application/json")
+    @POST("abstractdisplayapi.php")
+    Call<AbstractResponse> getAbstractDisplay(@Body JsonObject jsonObject);
+
+    @Headers("Content-Type: application/json")
+    @POST("vol_issueapi.php")
+    Call<VolumeIssueResponse> getVolumeIssueDisplay(@Body JsonObject jsonObject);
+
+    @Headers("Content-Type: application/json")
+    @POST("contactpagejournalsapi.php")
+    Call<JournalsListResponse> getJournalListDisplay(@Body JsonObject jsonObject);
+
+
+    @Headers("Content-Type: application/json")
+    @POST("contactapi.php")
+    Call<ContactResponse> getContactDisplay(@Body JsonObject jsonObject);
+
+    @Headers("Content-Type: application/json")
+    @POST("editorialboardapi.php")
+    Call<EditorialBoardResponse> getEditorialList(@Body JsonObject jsonObject);
+
+
+}
